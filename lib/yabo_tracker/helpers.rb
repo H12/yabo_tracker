319677e610fad9e58d637a24e0cc9320d4333e20
Henry Firth <henryfirth@Henrys-MacBook-Pro-2.local>
2016-03-19 13:32:08 -0500

    Fix table scraping

--- lib/yabo_tracker/helpers.rb
+++ lib/yabo_tracker/helpers.rb
@@ -4,11 +4,11 @@ require "nokogiri"
 module Helpers
   BASE_URL = "http://www.hittrackeronline.com/detail.php"
 
-  def build_base_url(id, type = "ballpark")
+  def self.build_base_url(id, type = "ballpark")
     BASE_URL + "?id=#{id}&type=#{type}"
   end
 
-  def generate_nokogiri_html(url)
+  def self.generate_nokogiri_html(url)
     Nokogiri::HTML(Net::HTTP.get(URI(url)))
   end
 end
--- lib/yabo_tracker/homerun.rb
+++ lib/yabo_tracker/homerun.rb
@@ -1,10 +1,22 @@
-require_relative "./helpers"
+require "net/http"
+require "nokogiri"
+require_relative "helpers"
 
 class Homerun
   attr_reader :true_distance, :horizontal_angle
 
+  include Helpers
+
   def initialize(homerun_nokogiri_row)
-    @true_distance    = homerun_nokogiri_row.search('td:nth-of-type(19) span').text.to_i
     @horizontal_angle = homerun_nokogiri_row.search('td:nth-of-type(14) span').text.to_f
+    @true_distance    = homerun_nokogiri_row.search('td:nth-of-type(19) span').text.to_i
+  end
+
+  def self.homers_for_id(id)
+    html = Helpers.generate_nokogiri_html(Helpers.build_base_url(id))
+    rows = html.search('table.forumline:last-of-type tr')[4..-1]
+    rows.map do |row|
+      Homerun.new(row)
+    end
   end
 end
